2ee61e91aa2aafed7b34eb171c4994e68eca9114
DivyanshKasera <divyanshjain380@gmail.com>
2025-08-22 07:16:33 +0000

    commit

--- arrays.cpp
+++ arrays.cpp
@@ -1,16 +1,19 @@
 #include<iostream>
 using namespace std;
 
+int linerSearch(int arr[], int size, int target){
+    for(int i= 0; i<size;i++){
+        if(arr[i]==target){
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main(){
-    int size = 5;
-int marks[size];
-
-for(int i = 0; i<size; i++){
-    cin>>marks[i];
-}
-
-for(int i = 0; i<size; i++){
-cout<<marks[i]<<endl;
-}
-return 0;
+    int arr[] = {2,4,1,7,5,10};
+    int size = 7;
+    int target = 8;
+    cout<<linerSearch(arr,size,target)<<endl;
+    return 0;
 }
