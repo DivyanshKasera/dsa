aa51c6421181d4d3f1dfb94199b45c9638aac74f
DivyanshKasera <divyanshjain380@gmail.com>
2025-08-22 08:59:08 +0000

    commit

--- arrays.cpp
+++ arrays.cpp
@@ -1,19 +1,23 @@
 #include<iostream>
 using namespace std;
 
-int linerSearch(int arr[], int size, int target){
-    for(int i= 0; i<size;i++){
-        if(arr[i]==target){
-            return i;
-        }
-    }
-    return -1;
+void reverseArray(int arr[], int size){
+    int start = 0, end = size-1;
+    while(start < end ){
+    swap(arr[start],arr[end]);
+    start++;
+    end--;
+}
 }
 
 int main(){
     int arr[] = {2,4,1,7,5,10};
-    int size = 7;
-    int target = 8;
-    cout<<linerSearch(arr,size,target)<<endl;
-    return 0;
+     int size = 6;
+     reverseArray(arr , size);
+
+     for(int i= 0; i<size;i++){
+        cout<<arr[i]<<" ";
+     }
+    cout<<endl;
+     return 0;
 }
