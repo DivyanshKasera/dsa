6b65029511f87a92ef47de018cfdce81c607cba8
DivyanshKasera <divyanshjain380@gmail.com>
2025-08-24 05:38:03 +0000

    commit

--- arrays.cpp
+++ arrays.cpp
@@ -6,21 +6,25 @@ vector<int>pairsum(vector<int> nums , int target){
     vector<int> ans;
     int n = nums.size();
 
-    for(int i=0; i<n; i++){
-        for(int j = i+1; j<n; j++){
-            if(nums[i] + nums[j] == target){
-                ans.push_back(i);
-                ans.push_back(j);
-                return ans;
-            }
+  int i=0, j = n-1;
+  while(i<j){
+    int pairsum = nums[i] + nums[j];
+    if(pairsum > target){
+        j--;
+    }else if(pairsum < target){
+            i++;
+        }else{
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
         }
-    }
+     }
     return ans;
 }
-
-     int main(){
+  
+  int main(){
         vector<int>nums ={2,3,7,11,15};
-        int target = 13;
+        int target = 26;
 
         vector<int>ans =  pairsum(nums , target);
         cout<<ans[0]<<", "<<ans[1]<<endl;
