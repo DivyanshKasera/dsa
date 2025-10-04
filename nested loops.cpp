dc0c7b4228e01ce0dc0c2e9253055bdadb1fcb2b
DivyanshKasera <divyanshjain380@gmail.com>
2025-10-04 12:19:37 +0000

    commit

--- nested loops.cpp
+++ nested loops.cpp
@@ -2,32 +2,19 @@
 using namespace std;
 
 int main(){
-  int n = 4;
-  
-  for(int i=0; i<n; i++){    //outer loop  top/lest side
-        for(int j=0; j<(i+1); j++){   //inner loop for stars
-            cout<<"* ";
-        }
-        for(int j=0; j<(2*(n-i)-2); j++){   //inner loop spaces
-            cout<<"  ";
-        }
-        for(int j=0; j<(i+1); j++){   //inner loop star
-            cout<<"* ";
-        }
-        cout<<endl;
-    }
-    for(int i=0; i<n; i++){   //outer loop  bottom/right side
-        for(int j=0; j<(n-i); j++){     //inner loop for stars
-            cout<<"* ";
-        }
-        for(int j=0; j<(2*i); j++){            //inner loop for spaces
-            cout<<"  ";
-        }
-        for(int j=0; j<(n-i); j++){           //inner loop for stars
-            cout<<"* "; 
-        }
-        cout<<endl;
-    }
+    int n;
+    cin>>n;
 
-  return 0;
-  }
+    int i = 1;
+   
+  
+    while(i<=n){
+      int j= 1;
+      while(j<=i){ 
+         cout<<"*";
+         j=j+1;   
+      }
+      cout<<endl;     
+      i=i+1;
+    }
+    }
