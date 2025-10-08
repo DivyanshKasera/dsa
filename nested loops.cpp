96fcff914155549ad943fb9197c95ff93ab94000
DivyanshKasera <divyanshjain380@gmail.com>
2025-10-08 04:30:46 +0000

    commit

--- nested loops.cpp
+++ nested loops.cpp
@@ -7,15 +7,26 @@ int main(){
 
     int i = 1;
     
-  
-    while(i<=n){
-      int j= 1;
-      while(j<=n){ 
-         char ch = ('A'+ i + j -2);
-         cout<<ch;  
-         j=j+1;   
+     while(i<=n){
+      int space = n - i;
+      while(space){
+      cout<<" ";
+      space = space -1;
+     }
+      int j = 1;
+      while(j<=i){
+      cout<<j;
+      j = j + 1;
       }
-      cout<<endl;     
-      i=i+1; 
-    }
-    }
+      int start = i-1;
+     while(start){
+      cout<<start;
+      start = start-1;
+       }
+     cout<<endl;
+      i = i + 1;
+      }
+     }
+      
+  
+    
